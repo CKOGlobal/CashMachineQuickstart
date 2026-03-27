828c2dbabe2bd86ca0764a0adaa7e98e3546a3bc
CKO <kellik73@yahoo.com>
2026-03-27 10:01:18 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CashMachineQuickStart from './CashMachineQuickStart';
+import CashMachineQuickstart from './CashMachineQuickstart';
 import StuckChat from './StuckChat';
+import CMQSOptIn from './CMQSOptIn';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CashMachineQuickStart />} />
+        <Route path="/" element={<CashMachineQuickstart />} />
         <Route path="/stuck-chat" element={<StuckChat />} />
+        <Route path="/cmqs-opt-in" element={<CMQSOptIn />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
